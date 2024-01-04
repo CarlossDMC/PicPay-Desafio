69018fd534e44c1ee0cb33b293516c4ec16d8cf5
CarlossDMC <carlosdaniel.mc@hotmail.com>
2024-01-03 23:27:42 -0300

    Começo da implementação de salvar um novo usuário e atualizar um já existente.

--- src/main/java/com/carlos/picpaydesafio/repositories/UserRepository.java
+++ src/main/java/com/carlos/picpaydesafio/repositories/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Usuario, Long> {
 
+    public Usuario findUsuarioById(Long id);
+
 
 }
